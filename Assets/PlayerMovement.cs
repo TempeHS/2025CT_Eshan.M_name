ceb65879cc50c6371c123a73289afd5d1565e079
Eshanmalik <eshan.malik@education.nsw.gov.au>
2025-05-26 10:33:06 +1000

    need to fix errors

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,32 +6,49 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumoingPower = 16f;
-    private bool isfaceingRight = true;
-
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
     void Update()
     {
-    horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
     }
+
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed,rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.Log("update");
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
     private void Flip()
-
     {
-    if (isFacingRight && horizontal < 0f || !isfaceingRight && horizontal > 0f)
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            isFaceingRight = !isFaceingRight;
-            Vector3
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
-
     }
 }
